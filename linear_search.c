6d0058ab470ce407e901b2221bf6102e2320e1bf
Akash Jain <akashjpr25@gmail.com>
2023-04-03 23:45:34 +0530

    Update linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -4,7 +4,7 @@ void main()
     int K=0, index=-1;
     int a[] = {1,2,3,4,5,6,7,8,9,10};
     
-    printf("Enter the element to be searched: \n");
+    printf("Enter the Element to be Searched: \n");
     scanf("%d", &K);
     for (int i=0; i<10; i++){
         if(a[i] == K)
@@ -16,5 +16,5 @@ void main()
     if (index != -1)
     printf("Element present at index: %d", index);
     else
-    printf("Element not present");
+    printf("Element not Present");
 }
